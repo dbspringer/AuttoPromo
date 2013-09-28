9078c2de4e35af0fe3c4be957ebe023070238b66
stuwest <stu@Stus-MacBook-Pro-Retina.local>
2013-09-28 15:33:39 -0700

    cleanup

--- test.php
+++ test.php
@@ -3,22 +3,22 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
-function chop_text($text) {
-	return explode("\n", wordwrap($text, 136, "\n"));
+function chop_text($text,$max_len2=136) {
+	return explode("\n", wordwrap($text, $max_len2, "\n"));
 }
 
 function max_len($text, $chunk_len=136) {
-	$chunk_count =  count(chop_text($text));
+	$chunk_count =  count(chop_text($text,$chunk_len));
 	echo $chunk_count.' chunks'."\n";
 	echo ceil(log10($chunk_count)).' digits in the chunk count'."\n";
 	if($chunk_count>9) {
 		$chunk_len = $chunk_len-2;
-		$chunk_count =  ceil($string_len / $chunk_len);
+		$chunk_count =  count(chop_text($text,$chunk_len));
 		echo ceil($chunk_count).' chunks'."\n";
 	}
 	if($chunk_count>99) {
 		$chunk_len = $chunk_len-2;
-		$chunk_count =  ceil($string_len / $chunk_len);
+		$chunk_count =  count(chop_text($text,$chunk_len));
 		echo ceil($chunk_count).' chunks'."\n";
 	}
 	echo $chunk_len.' chunk length'."\n";
@@ -27,7 +27,7 @@ function max_len($text, $chunk_len=136) {
 
 
 function recursive_max_len($text,$max_len=136,$n=1) {
-  if (count(chop_text($text))/$max_len<$n) { // our base case
+  if (count(chop_text($text,$max_len))/$max_len<$n) { // our base case
      return $max_len;
   }
   else {
@@ -36,9 +36,9 @@ function recursive_max_len($text,$max_len=136,$n=1) {
 }
 
 echo "\n".'Our sample text:'."\n";
-print_r(chop_text(file_get_contents('./KJV.txt', true)));
-max_len(file_get_contents('./KJV.txt', true));
-echo recursive_max_len(file_get_contents('./KJV.txt', true)).' is the recursive max length'."\n";
+print_r(chop_text(file_get_contents('./creed.txt', true)));
+max_len(file_get_contents('./creed.txt', true));
+echo recursive_max_len(file_get_contents('./creed.txt', true)).' is the recursive max length'."\n";
 
 // echo "\n".'The King James Version of the Bible:'."\n";
 // max_len(file_get_contents('./KJV.txt', true));
