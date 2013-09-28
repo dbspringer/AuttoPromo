eee665fd7b890e5f4697a047a67b5bb07d7fc25c
Derek Springer <derekspringer@gmail.com>
2013-09-28 16:03:47 -0700

    Changed name back to $max_len.

--- test.php
+++ test.php
@@ -3,19 +3,19 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
-function chop_text($text,$max_len2=136) {
-	return explode("\n", wordwrap($text, $max_len2, "\n"));
+function chop_text($text,$max_len=136) {
+	return explode("\n", wordwrap($text, $max_len, "\n"));
 }
 
-function max_len2( $text_len, $max_len = 136, $n = 1 ) {
-	if($text_len < $max_len)
+function max_len( $text_len, $max_len = 136, $n = 1 ) {
+	if ( $text_len < $max_len )
 		return $max_len;
-	if($text_len/$max_len > $n)
-		return max_len2($text_len, $max_len-2, $n*10);
+	if ( $text_len/$max_len > $n )
+		return max_len( $text_len, $max_len-2, $n*10 );
 	else
 		return $max_len;
 }
 
-echo "\n".'Tweet contents:'."\n";
+echo "\nTweet contents:\n";
 print_r(chop_text(file_get_contents('./KJV.txt', true)));
-echo max_len2( strlen(file_get_contents('./KJV.txt', true)))." chunk length"."\n";
+echo max_len( strlen(file_get_contents('./KJV.txt', true))) . " chunk length\n";
