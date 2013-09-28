c9735b1533c5f8b970f6c8eef3006e3ccd560bea
stuwest <stu@Stus-MacBook-Pro-Retina.local>
2013-09-28 15:26:30 -0700

    working recursive function (almost)

--- test.php
+++ test.php
@@ -26,9 +26,19 @@ function max_len($text, $chunk_len=136) {
 }
 
 
+function recursive_max_len($text,$max_len=136,$n=1) {
+  if (count(chop_text($text))/$max_len<$n) { // our base case
+     return $max_len;
+  }
+  else {
+     return recursive_max_len($text,$max_len-2,$n*10); // <--calling itself.
+  }
+}
+
 echo "\n".'Our sample text:'."\n";
-print_r(chop_text(file_get_contents('./creed.txt', true)));
-max_len(file_get_contents('./creed.txt', true));
+print_r(chop_text(file_get_contents('./KJV.txt', true)));
+max_len(file_get_contents('./KJV.txt', true));
+echo recursive_max_len(file_get_contents('./KJV.txt', true)).' is the recursive max length'."\n";
 
 // echo "\n".'The King James Version of the Bible:'."\n";
 // max_len(file_get_contents('./KJV.txt', true));
