5ed35d8884937f4596c0b54fe17e59834b2e07ad
stuwest <stu@Stus-MacBook-Pro-Retina.local>
2013-09-28 15:15:49 -0700

    cleanup

--- test.php
+++ test.php
@@ -3,9 +3,6 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
-$sample = "Automattic Creed\nI will never stop learning. I won’t just work on things that are assigned to me. I know there’s no such thing as a status quo. I will build our business sustainably through passionate and loyal customers. I will never pass up an opportunity to help out a colleague, and I’ll remember the days before I knew everything. I am more motivated by impact than money, and I know that Open Source is one of the most powerful ideas of our generation. I will communicate as much as possible, because it’s the oxygen of a distributed company. I am in a marathon, not a sprint, and no matter how far away the goal is, the only way to get there is by putting one foot in front of another every day. Given time, there is no problem that’s insurmountable.";
-print_r($output);
-
 function chop_text($text) {
 	return explode("\n", wordwrap($text, 136, "\n"));
 }
@@ -38,6 +35,7 @@ function recursive_max_len($text, $chunk_len, $n=10) {
 }
 
 echo "\n".'Our sample text:'."\n";
+print_r(chop_text(file_get_contents('./creed.txt', true)));
 max_len($sample);
 
 // echo "\n".'The King James Version of the Bible:'."\n";
