ef5b9e34a84f61cc976b68971a0c1b9229b0f68e
stuwest <stu@Stus-MacBook-Pro-Retina.local>
2013-09-28 18:00:50 -0700

    fixed global/local variable issue & reordered

--- test.php
+++ test.php
@@ -4,10 +4,6 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 $file=file_get_contents('./creed.txt', true);
 
-function chop_text($text,$max_len=136) {
-	return explode("\n", wordwrap($text, $max_len, "\n"));
-}
-
 function max_len( $text_len, $max_len = 136, $n = 1 ) {
 	if ( $text_len < $max_len )
 		return $max_len;
@@ -17,20 +13,25 @@ function max_len( $text_len, $max_len = 136, $n = 1 ) {
 		return $max_len;
 }
 
-echo "\nTweet contents:\n"; // for testing
+function chop_text($text,$max_len=136) {
+	return explode("\n", wordwrap($text, $max_len, "\n"));
+}
+
+function append_counts($array,$how_many) {
+	$tweets_to_send = array();
+	$counter=1;
+	foreach ($array as $value) {
+		$tweets_to_send[]= "$value $counter/$how_many";
+	    $counter++;
+	}
+	print_r($tweets_to_send);
+}
+
 print_r(chop_text($file)); // for testing
 $chunk_length=max_len( strlen($file));
 echo "Chunk length: ".$chunk_length."\n"; // for testing
 $tweet_count=count(chop_text($file,$chunk_length));
 echo "Tweet count: ".$tweet_count."\n"; // for testing
-
-function append_counts($array) {
-	$counter=1;
-	$tweets_to_send = array();
-	foreach (chop_text($file) as $value) {
-		$tweets_to_send[]= "$value $counter/$tweet_count";
-	    $counter++;
-	}
-}
-
-print_r(append_counts(chop_text($file,$chunk_length)));
+echo "Tweet contents:\n"; // for testing
+append_counts(chop_text($file,$chunk_length),$tweet_count);
+?>
